50b2727d974dcc54f73e948caf4a5d34f6e6e341
ashiqurrah7 <ashiqur.rahm@gmail.com>
2021-06-13 03:22:20 +0600

    added indexes to each cell

--- src/Board/Board.tsx
+++ src/Board/Board.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react'
 import './board.css'
 
-const BOARD_SIZE = 10;
+const BOARD_SIZE: number = 10;
+
+class Node{
+    val: number;
+    next: Node | null;
+
+    constructor(val:number){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class LinkedList{
+    head: Node;
+    tail: Node;
+    constructor(val:number){
+        const node = new Node(val);
+        this.head = node;
+        this.tail = node;
+    }
+}
 
 export const Board = () => {
 
-    const [board, setboard] = useState(
-        new Array(BOARD_SIZE).fill(0).map(row  => new Array(BOARD_SIZE).fill(0)),
-    );
+    const [board, setboard] = useState(createBoard(BOARD_SIZE));
 
     return (
         <div className="board">
@@ -15,7 +33,9 @@ export const Board = () => {
                 board.map((row, rowIdx) => 
                     <div key={rowIdx} className="row">
                         {row.map((cellIdx) => 
-                            <div key={cellIdx} className="cell"></div>
+                            <div key={cellIdx} className={`cell  ${false? 'snake-cell' : ''}`}>
+                                {cellIdx}
+                            </div>
                         )
                         }
                     </div>
@@ -23,4 +43,17 @@ export const Board = () => {
             }
         </div>
     )
+}
+
+const createBoard = (BOARD_SIZE: number) => {
+    let count = 1;
+    const board: number[][] = [];
+    for(let row=0; row<BOARD_SIZE; row++){
+        let currentRow:number[] = []
+        for (let cell=0; cell<BOARD_SIZE; cell++){
+            currentRow.push(count++);
+        }
+        board.push(currentRow);
+    }
+    return board;
 }
